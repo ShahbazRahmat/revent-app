2d1f4f32a8b2bd9d31701b3a52b3710b1b19b512
DESKTOP-GMN0RKC\Shahbaz <shahbazrahmat@gmail.com>
2018-10-06 00:59:01 -0700

    Initial commit

---
+++ src/App/Layout/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>This is App component</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
