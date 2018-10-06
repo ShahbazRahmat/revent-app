bac71cc07459494b27c05a7072ba4738e4f6d82a
DESKTOP-GMN0RKC\Shahbaz <shahbazrahmat@gmail.com>
2018-10-06 14:00:40 -0700

    strature changes layout complete

--- src/App/Layout/App.jsx
+++ src/App/Layout/App.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { Container } from 'semantic-ui-react';
+import EventDashboard from '../../Features/event/EventDashboard/EventDashboard';
+import NavBar from '../../Features/nav/NavBar/NavBar';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h2>This is App component</h2>
+      <div>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
       </div>
     );
   }
