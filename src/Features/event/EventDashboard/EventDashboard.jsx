78d5c1d7d3cf9cb4e2c2113fe0e8c6af02b0f50d
DESKTOP-GMN0RKC\Shahbaz <shahbazrahmat@gmail.com>
2018-10-17 11:34:10 -0700

    deleteEvent

--- src/Features/event/EventDashboard/EventDashboard.jsx
+++ src/Features/event/EventDashboard/EventDashboard.jsx
@@ -108,13 +108,22 @@ class EventDashboard extends Component {
       isOpen: false
     })
   }
+  
+  handleDeleteEvent = (eventId) => () =>{
+    const updatedEvents = this.state.events.filter(e => e.id !== eventId);
+    this.setState({ 
+      events: updatedEvents
+    })
+  }
+
+
 
   render() {
     const { selectedEvent } = this.state;
     return (
       <Grid>
         <Grid.Column width={10}>
-          <EventList onEventOpen={this.handleOpenEvent} events={this.state.events} />
+          <EventList deleteEvent={this.handleDeleteEvent} onEventOpen={this.handleOpenEvent} events={this.state.events} />
         </Grid.Column>
         <Grid.Column width={6}>
           <Button
