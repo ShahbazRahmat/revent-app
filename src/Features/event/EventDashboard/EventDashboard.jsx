e49c68933f11b6656d4a866ec83df68deed44ec2
DESKTOP-GMN0RKC\Shahbaz <shahbazrahmat@gmail.com>
2019-04-02 22:52:35 +0500

    Adding initial redux

--- src/Features/event/EventDashboard/EventDashboard.jsx
+++ src/Features/event/EventDashboard/EventDashboard.jsx
@@ -1,63 +1,13 @@
 import React, { Component } from 'react';
 import { Grid, Button } from 'semantic-ui-react';
+import { connect } from 'react-redux';
 import cuid from 'cuid';
 import EventList from '../EventList/EventList';
 import EventForm from '../EventForm/EventForm';
-
-const eventsDashboard = [
-  {
-    id: '1',
-    title: 'Trip to Tower of London',
-    date: '2018-03-27',
-    category: 'culture',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
-    city: 'London, UK',
-    venue: "Tower of London, St Katharine's & Wapping, London",
-    hostedBy: 'Bob',
-    hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
-    attendees: [
-      {
-        id: 'a',
-        name: 'Bob',
-        photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
-      },
-      {
-        id: 'b',
-        name: 'Tom',
-        photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
-      }
-    ]
-  },
-  {
-    id: '2',
-    title: 'Trip to Punch and Judy Pub',
-    date: '2018-03-28',
-    category: 'drinks',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
-    city: 'London, UK',
-    venue: 'Punch & Judy, Henrietta Street, London, UK',
-    hostedBy: 'Tom',
-    hostPhotoURL: 'https://randomuser.me/api/portraits/men/22.jpg',
-    attendees: [
-      {
-        id: 'b',
-        name: 'Tom',
-        photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
-      },
-      {
-        id: 'a',
-        name: 'Bob',
-        photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
-      }
-    ]
-  }
-];
+//import { createEvent, } from '../eventActions';
 
 class EventDashboard extends Component {
   state = {
-    events: eventsDashboard,
     isOpen: false,
     selectedEvent: null
   };
@@ -116,13 +66,15 @@ class EventDashboard extends Component {
 
   render() {
     const { selectedEvent } = this.state;
+    const { events } = this.props;
+    debugger;
     return (
       <Grid>
         <Grid.Column width={10}>
           <EventList
             deleteEvent={this.handleDeleteEvent}
             onEventOpen={this.handleOpenEvent}
-            events={this.state.events}
+            events={events}
           />
         </Grid.Column>
         <Grid.Column width={6}>
@@ -145,4 +97,12 @@ class EventDashboard extends Component {
   }
 }
 
-export default EventDashboard;
+const mapDispatchToProps = {};
+const mapStateToProps = state => ({
+  events: state.events
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(EventDashboard);
--- src/Features/testarea/testComponent.jsx
+++ src/Features/testarea/testComponent.jsx
@@ -3,15 +3,13 @@ import { connect } from 'react-redux';
 import { Button } from 'semantic-ui-react';
 import { incrementCounter, decrementCounter } from './testActions';
 
-const actions = { incrementCounter, decrementCounter };
-
 class testComponent extends Component {
   render() {
     const { incrementCounter, decrementCounter, data } = this.props;
     return (
       <div>
         <h2>Test Area</h2>
-        <h3>this is state: {this.props.data}</h3>
+        <h3>this is state: {data}</h3>
         <Button onClick={incrementCounter} color="green" content="Increment" />
         <Button onClick={decrementCounter} color="red" content="Decrement" />
       </div>
@@ -19,11 +17,12 @@ class testComponent extends Component {
   }
 }
 
+const mapDispatchToProps = { incrementCounter, decrementCounter };
 const mapStateToProps = state => ({
   data: state.test.data
 });
 
 export default connect(
   mapStateToProps,
-  actions
+  mapDispatchToProps
 )(testComponent);
